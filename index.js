553a53635912eb29f105f2d892ec2fb8cf6bf222
Joe Warner <joewarnerdev@gmail.com>
2020-01-01 12:42:56 -0600

    added play again option
    to make easier to show off to friends

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 6;
-const cellsVertical = 8;
+const cellsHorizontal = 15;
+const cellsVertical = 12;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
